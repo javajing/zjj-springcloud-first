12c77a0a8f202defad2fb462859e0d39a806bf68
zhangjingjing <jingjing.zhang@dmall.com>
2021-04-14 10:57:54 +0800

    [zjj]1.eureka节点1;

---
+++ myfirst-eureka-node2/src/main/java/com/wh/zjj/EurekaAppNode2.java
@@ -0,0 +1,34 @@
+package com.wh.zjj;
+
+import org.springframework.boot.Banner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author zhangjingjing
+ * @date 2021/4/13 20:14
+ * @project springcloudfirst
+ * @desc (eureka的节点2)
+ */
+@EnableEurekaServer//EurekaServer服务器端启动类,接受其它微服务注册进来
+@SpringBootApplication
+public class EurekaAppNode2 extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(EurekaAppNode2.class);
+        application.setBannerMode(Banner.Mode.OFF);
+        application.run(args);
+    }
+
+    /**
+     * 支持外部容器启动
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(EurekaAppNode2.class);
+    }
+
+}
